a93b6930d829e18b262dacffeb9a4e1e1e696702
dudeface67 <thatborg@gmail.com>
2017-01-29 10:25:12 +0000

    added .gitignore file

--- https:/github.com/dudeface67/MIlan_scores.git/Score retrieval.py
+++ https:/github.com/dudeface67/MIlan_scores.git/Score retrieval.py
@@ -1,6 +1,10 @@
+#so far i just want to scrape the live scores website, haven't thought past that
+
 import requests
 
 all_scores = 'http://www.livescores.com/'
 headers = {'user_agent': 'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.91 Safari/537.36'}
 response = requests.get(all_scores, headers=headers)
 response.status_code
+
+# The status code is just a function to ensure that  the website is working, nothing is being
