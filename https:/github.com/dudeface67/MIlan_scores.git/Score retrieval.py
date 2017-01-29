c0653309cfc8d1204668e3abcce8ebe7aaeb6e5b
dudeface67 <thatborg@gmail.com>
2017-01-29 10:34:09 +0000

    added .gitignore file

--- https:/github.com/dudeface67/MIlan_scores.git/Score retrieval.py
+++ https:/github.com/dudeface67/MIlan_scores.git/Score retrieval.py
@@ -7,4 +7,4 @@ headers = {'user_agent': 'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KH
 response = requests.get(all_scores, headers=headers)
 response.status_code
 
-# The status code is just a function to ensure that  the website is working, nothing is being
+# The status code is just a function to ensure that the website is working, nothing is being returned.... something wrong with user agent?
