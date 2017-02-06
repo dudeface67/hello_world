61d63a4617107d9de1386e3de8f0204739f6fbac
dudeface67 <thatborg@gmail.com>
2017-02-06 11:53:30 +0000

    added .gitignore file

--- wikiretrieve.py
+++ wikiretrieve.py
@@ -1,10 +1,19 @@
+# I just want it to retrieve the the featured article
+
 import urllib.request
+import urllib.parse
+import re
 
+#user agent required because wikipedia blocks the python user agent :(
 
-url = 'https://en.wikipedia.org/'
+url = 'https://en.wikipedia.org/wiki/Main_Page'
 headers = {}
 headers ['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/600.3.18 (KHTML, like Gecko) Version/8.0.3 Safari/600.3.18'
 req = urllib.request.Request(url,headers = headers)
 resp = urllib.request.urlopen(req)
 respdata = resp.read()
-print (respdata)
+#print (respdata)
+
+article = re.findall(r'<p>(.*?)</p>', str(respdata))
+for p in article:
+    print (p)
