1412abe2a1cffeed3313ce7963413ff45096c0b3
dudeface67 <thatborg@gmail.com>
2017-02-07 19:14:21 +0000

    added .gitignore file

--- wikiretrieve.py
+++ wikiretrieve.py
@@ -11,16 +11,16 @@ url = 'https://en.wikipedia.org/wiki/Main_Page'
 headers = {}
 headers ['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/600.3.18 (KHTML, like Gecko) Version/8.0.3 Safari/600.3.18'
 
-def get_article(featured):
+def get_article():
     webpage = urllib.request.urlopen(url, headers = headers).read()
     parser = AdvancedHTMLParser()
     parser.parseStr(webpage)
     featured_article = parser.getElementById(art_id)
-    print (featured_article)
+    return (featured_article)
 
 art_id = "mp-tfa"
-
-
+comeon = get_article(art_id)
+print (comeon)
 '''
 req = urllib.request.Request(url,headers = headers)
 resp = urllib.request.urlopen(req)
