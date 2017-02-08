5f7c71a26c1d4d43b70b0f13a88b1558362b9ad3
dudeface67 <thatborg@gmail.com>
2017-02-08 21:01:29 +0000

    adding some files

--- wikiretrieve.py
+++ wikiretrieve.py
@@ -4,23 +4,24 @@ import urllib.request
 import urllib.parse
 #import re
 from AdvancedHTMLParser import AdvancedHTMLParser
+from AdvancedHTMLParser.Tags import AdvancedTag
 
-#user agent required because wikipedia blocks the python user agent :(
+#user agent not required, thought they were but im just bad :D
 
 url = 'https://en.wikipedia.org/wiki/Main_Page'
-headers = {}
-headers ['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/600.3.18 (KHTML, like Gecko) Version/8.0.3 Safari/600.3.18'
+#headers = {}
+#headers ['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/600.3.18 (KHTML, like Gecko) Version/8.0.3 Safari/600.3.18'
 
 def get_article():
-    webpage = urllib.request.urlopen(url, headers = headers).read()
+    webpage = urllib.request.urlopen(url).read()
     parser = AdvancedHTMLParser()
     parser.parseStr(webpage)
-    featured_article = parser.getElementById(art_id)
-    return (featured_article)
+    featured_article = parser.getElementById("mp-tfa").getChildren().getElementsByTagName("p")
+    print (featured_article)
 
-art_id = "mp-tfa"
-comeon = get_article(art_id)
-print (comeon)
+get_article()
+
+
 '''
 req = urllib.request.Request(url,headers = headers)
 resp = urllib.request.urlopen(req)
